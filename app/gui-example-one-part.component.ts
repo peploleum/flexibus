cc1f20466e217ccd31fdbd5be06a507fac46a153
peploleum <peploleum@gmail.com>
2016-08-12 11:37:05 +0200

    added definitelyTyped d3 v3.0.0 component

--- app/gui-example-one-part.component.ts
+++ app/gui-example-one-part.component.ts
@@ -1,5 +1,5 @@
 ﻿import {Component, OnInit} from '@angular/core';
-
+import * as d3 from 'd3';
 @Component({
     selector: 'part-one',
     templateUrl: 'app/gui-example-one-part.html'
@@ -7,6 +7,17 @@
 export class GuiExampleOnePartComponent implements OnInit {
 
     ngOnInit() {
+        var selectedSvgAnchor = d3.select('#svganchor');
+        //Make an SVG Container
+        var svgContainer = selectedSvgAnchor.append("svg")
+            .attr("width", 200)
+            .attr("height", 200);
+
+        //Draw the Circle
+        var circle = svgContainer.append("circle")
+            .attr("cx", 30)
+            .attr("cy", 30)
+            .attr("r", 20);
     }
 
 
--- systemjs.config.js
+++ systemjs.config.js
@@ -9,14 +9,16 @@
         '@angular': 'node_modules/@angular',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs': 'node_modules/rxjs',
-        'openlayers': 'node_modules/openlayers/dist'
+        'openlayers': 'node_modules/openlayers/dist',
+        'd3' : 'node_modules/d3'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': {main: 'main.js', defaultExtension: 'js'},
         'rxjs': {defaultExtension: 'js'},
         'angular2-in-memory-web-api': {main: 'index.js', defaultExtension: 'js'},
-        'openlayers': {main: 'ol-debug.js', defaultExtension: 'js'}
+        'openlayers': {main: 'ol-debug.js', defaultExtension: 'js'},
+        'd3' : {main: 'd3.js'}
     };
     var ngPackageNames = [
         'common',
