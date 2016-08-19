a22e4a170a6ff7cfc55e9cce868a86fe2a1bc0d8
anaxa-gore <thomas.bonavia@hotmail.fr>
2016-08-19 10:44:32 +0200

    Insérer les composants au bon endroit dans le DOM

--- app/main-container.component.ts
+++ app/main-container.component.ts
@@ -1,11 +1,6 @@
-﻿import {
-    Component,
-    OnInit,
-    ComponentFactoryResolver,
-    ViewContainerRef,
-} from '@angular/core';
-import {GuiContainer} from './gui/gui-container';
-import {GuiManagerService} from './gui/gui-manager.service';
+﻿import {Component, OnInit, ComponentFactoryResolver, ViewContainerRef, ViewChild} from "@angular/core";
+import {GuiContainer} from "./gui/gui-container";
+import {GuiManagerService} from "./gui/gui-manager.service";
 import {GuiView} from "./gui/gui-view.component";
 //import * as components from './index';
 
@@ -28,18 +23,20 @@ var customEntryComponents = [];
 // 4 - navbar / sidebar
 // 5 - try AOT instead of JIT compilation http://angularjs.blogspot.fr/2016/08/angular-2-rc5-ngmodules-lazy-loading.html
 export class MainContainerComponent implements OnInit {
+    @ViewChild('mainAnchor', {read: ViewContainerRef}) anchor:ViewContainerRef;
 
-    constructor(private cfr:ComponentFactoryResolver, private vcr:ViewContainerRef, private gms:GuiManagerService) {
+    constructor(private cfr:ComponentFactoryResolver, private gms:GuiManagerService) {
     }
 
     ngOnInit() {
+        console.log("Main View initialisée");
         let containers:Array<GuiContainer> = this.gms.getGuiContainers();
 
         for (var container of containers) {
             console.log('trying to inject :) ' + container.mainItem.name + ' component in the container');
 
             let view = this.cfr.resolveComponentFactory(GuiView);
-            let viewComponentRef = this.vcr.createComponent(view);
+            let viewComponentRef = this.anchor.createComponent(view);
 
             viewComponentRef.instance.setMain(container.mainItem.componentType);
             for (var leftItem of container.leftItems) {
@@ -48,12 +45,6 @@ export class MainContainerComponent implements OnInit {
             for (var rightItem of container.rightItems) {
                 viewComponentRef.instance.addRight(rightItem.componentType);
             }
-
-
-            // angular2 needs 'entryComponents' to be declared in a Component to enable dynamic component loading
-            /*let customComp = container.mainItem.componentType;
-             let resolvedComponentFactory = this.cfr.resolveComponentFactory(customComp);
-             this.vcr.createComponent(resolvedComponentFactory);*/
         }
     }
 
