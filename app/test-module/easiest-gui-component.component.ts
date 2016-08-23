f5c026b63b4e505b26eecf6559d9e7d89f1cf6ee
anaxa-gore <thomas.bonavia@hotmail.fr>
2016-08-23 16:49:46 +0200

    manage overflow in side panels

--- app/test-module/easiest-gui-component.component.ts
+++ app/test-module/easiest-gui-component.component.ts
@@ -5,7 +5,7 @@ import {Component} from "@angular/core";
 import {GuiComponent} from "../gui/gui-component";
 @Component({
     selector: 'easy',
-    template:'<div>Coucou !!!</div>'
+    template:'<div>Coucou !!!<br>ca va ? <br>moi oui mais je suis malade....<br>Sinon c\'est le pied les layouts toute la journée ça fait vraiment plaisir<br>Bon sang on est au bout du rouleau quoi... C\'est la merde coco.........<br>Bon sang on est au bout du rouleau quoi... C\'est la merde coco.........<br>Bon sang on est au bout du rouleau quoi... C\'est la merde coco.........</div>'
 })
 export class EasyComponent extends GuiComponent {
     
